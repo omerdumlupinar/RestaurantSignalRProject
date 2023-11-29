355bbb8ae18c43ec255a026cbd6ce1aa811c4819
Ömer Dumlupınar <omerdumlupinar0@gmail.com>
2023-11-29 23:20:35 +0300

    Dto Completed

---
+++ RestaurantSignalRProject.DtoLayer/ContentDto/GetContentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSignalRProject.DtoLayer.ContentDto
+{
+    public class GetContentDto
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+    }
+}
