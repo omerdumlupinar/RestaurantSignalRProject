32e5c63c98db485d75bf0661ecb367027ec43a9f
Ömer Dumlupınar <omerdumlupinar0@gmail.com>
2023-12-12 22:42:30 +0300

    ...

--- RestaurantSignalRProject.WebApi/Controllers/AboutController.cs
+++ RestaurantSignalRProject.WebApi/Controllers/AboutController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestaurantSignalRProject.BusinessLayer.Abstract;
+using RestaurantSignalRProject.DtoLayer.AboutDto;
+using RestaurantSignalRProject.EntityLayer.Entities;
 
 namespace RestaurantSignalRProject.WebApi.Controllers
 {
@@ -7,5 +11,59 @@ namespace RestaurantSignalRProject.WebApi.Controllers
     [ApiController]
     public class AboutController : ControllerBase
     {
+
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+
+        public AboutController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        [Route("AboutList")]
+        public IActionResult AboutList()
+        {
+            GetAboutDto getAboutDto = _mapper.Map<GetAboutDto>(_aboutService.TGetListAll());
+            return Ok(getAboutDto);
+        }
+
+        [HttpGet]
+        [Route("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+            GetAboutDto getAboutDto = _mapper.Map<GetAboutDto>(_aboutService.TGetById(id));
+            return Ok(getAboutDto);
+        }
+
+
+        [HttpGet]
+        [Route("CreateAbout")]
+        public IActionResult CreateAbout( CreateAboutDto createAboutDto)
+        {
+            About about = _mapper.Map<About>(createAboutDto);
+             _aboutService.TAdd(about);
+            return Ok("Hakkımızda ekleme işlemi başarılı.");
+        }
+
+        [HttpPut]
+        [Route("CreateAbout")]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = _mapper.Map<About>(updateAboutDto);
+            _aboutService.TUpdate(about);
+            return Ok("Hakkımızda güncelleme işlemi başarılı.");
+        }
+        [HttpDelete]
+        [Route("DeleteAbout")]
+        public IActionResult DeleteAbout(int id)
+        {
+            var entity=_aboutService.TGetById(id);
+            _aboutService.TDelete(entity);
+            return Ok("Hakkımızda silme işlemi başarılı.");
+        }
+
     }
 }
